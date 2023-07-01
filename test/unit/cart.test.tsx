033ae272d2bd93873cbeb29e8b3154c6b1769808
nikitanosov1 <nikinosov2002@mail.ru>
2023-07-01 17:09:12 +0400

    checkpoint

--- test/unit/cart.test.tsx
+++ test/unit/cart.test.tsx
@@ -8,6 +8,7 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import events from "@testing-library/user-event";
+import { Cart } from "../../src/client/pages/Cart";
 
 const basename = "/hw/store";
 
@@ -15,20 +16,30 @@ const api = new ExampleApi(basename);
 const cart = new CartApi();
 
 describe("Проверка корзины", () => {
-  // it("в шапке рядом со ссылкой на корзину должно отображаться количество не повторяющихся товаров в ней", async () => {
-  //   const store = initStore(api, cart);
-  //   const cartPage = (
-  //     <MemoryRouter initialEntries={["/hw/store/cart"]} initialIndex={0}>
-  //       <Provider store={store}>
-  //         <Application />
-  //       </Provider>
-  //     </MemoryRouter>
-  //   );
-  //   const { container, getByTestId } = render(app);
-  //   const tableBody = getByTestId("table-body") as HTMLElement;
-  //   const firstProduct = getByTestId("0") as HTMLElement;
-  //   const secondProduct = getByTestId("1") as HTMLElement;
-  //   const thirdProduct = getByTestId("2") as HTMLElement;
-  //   const catalogLinkFromGamburger = getByTestId("catalog-link") as HTMLElement;
-  // });
+  it("в шапке рядом со ссылкой на корзину должно отображаться количество не повторяющихся товаров в ней", async () => {
+    const store = initStore(api, cart);
+    const cartPage = (
+      <MemoryRouter initialEntries={["/hw/store/cart"]} initialIndex={0}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </MemoryRouter>
+    );
+    const renderedCart = render(cartPage);
+    const tableBody = renderedCart.getByTestId("table-body") as HTMLElement;
+
+    const catalogPage = (
+      <MemoryRouter initialEntries={["/hw/store/catalog"]} initialIndex={0}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </MemoryRouter>
+    );
+    const renderedCatalog = render(catalogPage);
+
+    const firstProduct = renderedCatalog.getByTestId("0") as HTMLElement;
+    const secondProduct = renderedCatalog.getByTestId("1") as HTMLElement;
+
+    const catalogLinkFromGamburger = getByTestId("catalog-link") as HTMLElement;
+  });
 });
