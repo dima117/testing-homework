0de6e5eb7f8e4a20ae50808ef139d96ef5a79089
nikitanosov1 <nikinosov2002@mail.ru>
2023-07-02 19:05:07 +0400

    added test for bug 5

--- test/hermione/kostili.hermione.js
+++ test/hermione/kostili.hermione.js
@@ -23,4 +23,40 @@ describe("Дополнительные костыльные проверки", a
       "Продукт не загрузился"
     );
   });
+
+  // BUG_ID = 5
+  it("После отправки формы в корзине появляется надпись об успехе", async function ({
+    browser,
+  }) {
+    await browser.url("/hw/store/catalog/0");
+    const button = await browser.$(".ProductDetails-AddToCart");
+    await button.waitForExist();
+    await button.click();
+
+    const catalogItemsMock = await browser.mock("**/hw/store/api/checkout");
+
+    await browser.url("/hw/store/cart");
+
+    const nameInput = await browser.$(".Form-Field_type_name");
+    await nameInput.waitForExist();
+    await nameInput.setValue("nikita");
+
+    const phoneInput = await browser.$(".Form-Field_type_phone");
+    await phoneInput.waitForExist();
+    await phoneInput.setValue("777877776676767");
+
+    const addressInput = await browser.$(".Form-Field_type_address");
+    await addressInput.waitForExist();
+    await addressInput.setValue("qwerty");
+
+    const formButton = await browser.$(".Form-Submit");
+    await formButton.waitForExist();
+
+    await formButton.click();
+
+    await browser.assertView("cartform", ".Application", {
+      compositeImage: true,
+      ignoreElements: [".Cart-SuccessMessage"],
+    });
+  });
 });
